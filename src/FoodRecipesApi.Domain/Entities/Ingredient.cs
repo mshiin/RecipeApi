20a579bd5d85e7b191a2cb1340a5a9625911defe
Michael Shin <mshin1303@gmail.com>
2025-08-10 14:03:46 +1000

    feat: add Ingredient Domain entity

---
+++ src/FoodRecipesApi.Domain/Entities/Ingredient.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodRecipesApi.Domain.Entities
+{
+    public class Ingredient
+    {
+        public int IngredientId {get;set;}
+
+    }
+}
