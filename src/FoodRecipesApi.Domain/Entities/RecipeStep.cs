e6383abebb6137095f720e5c5039499bc260fa46
Michael Shin <mshin1303@gmail.com>
2025-08-10 16:28:51 +1000

    feat: add one to many relationship by efs convention between RecipeStep and Recipe

--- src/FoodRecipesApi.Domain/Entities/RecipeStep.cs
+++ src/FoodRecipesApi.Domain/Entities/RecipeStep.cs
@@ -8,6 +8,8 @@ namespace FoodRecipesApi.Domain.Entities
     public class RecipeStep
     {
         public int RecipeStepId { get; set; }
+        public Recipe? Recipe { get; set; }
+        public int RecipeId { get; set; }
         public string Instruction { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
     
