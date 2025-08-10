92ff5547543a22fd5e3bd68a648aba21486c2a5c
Michael Shin <mshin1303@gmail.com>
2025-08-10 14:40:19 +1000

    feat: change Author property type to Author Model

--- src/FoodRecipesApi.Domain/Entities/Recipe.cs
+++ src/FoodRecipesApi.Domain/Entities/Recipe.cs
@@ -14,7 +14,12 @@ namespace FoodRecipesApi.Domain.Entities
         public int RecipeId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string Author { get; set; }
+        public Author Author { get; set; }
+        public ICollection<RecipeStep> RecipeSteps { get; private set; }
+        public string ImageUrl { get; set; }
+        public TimeSpan PreparationTime { get; set; }
+        public TimeSpan TotalTime { get; set; }
+        public ICollection<Ingredient> Ingredients { get; set; }
 
     }
 }
