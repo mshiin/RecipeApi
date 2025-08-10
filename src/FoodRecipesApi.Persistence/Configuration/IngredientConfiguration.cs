ed226ae44e56ed289391022e419a8236ae199056
Michael Shin <mshin1303@gmail.com>
2025-08-10 17:03:52 +1000

    feat: add ef fluentapi ingredient config class

---
+++ src/FoodRecipesApi.Persistence/Configuration/IngredientConfiguration.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodRecipesApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FoodRecipesApi.Persistence.Configuration
+{
+    public class IngredientConfiguration : IEntityTypeConfiguration<Ingredient>
+
+    {
+        public void Configure(EntityTypeBuilder<Ingredient> builder)
+        {
+            builder.HasOne(i => i.Quantity)
+                .WithOne(q => q.Ingredient)
+                .HasForeignKey<Ingredient>(i => i.QuantityId)
+                .IsRequired();
+                
+            builder.Property(i => i.Name)
+                .HasMaxLength(50)
+                .IsRequired();
+        }
+        
+    }
+}
