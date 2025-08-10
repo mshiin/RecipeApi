328e0160ae226324f8046a4c8816efc0a1fa7a9f
Michael Shin <mshin1303@gmail.com>
2025-08-10 14:33:07 +1000

    feat: add Author Domain entity

---
+++ src/FoodRecipesApi.Domain/Entities/Author.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodRecipesApi.Domain.Entities
+{
+    public class Author
+    {
+        public Author()
+        {
+            Recipes = new List<Recipe>();
+        }
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime Born { get; set; }
+        public TimeSpan Age => Born - DateTime.Now;
+        public ICollection<Recipe> Recipes { get; private set; }
+    }
+}
