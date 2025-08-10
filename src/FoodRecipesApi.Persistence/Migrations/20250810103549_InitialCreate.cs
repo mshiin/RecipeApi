1bfbd51535a7db5c5a41bc5c82ac3f1e6d377d9a
Michael Shin <mshin1303@gmail.com>
2025-08-10 20:39:17 +1000

    feat:generate UpdateDB initial migrate sql script

---
+++ src/FoodRecipesApi.Persistence/Migrations/20250810103549_InitialCreate.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodRecipesApi.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    AuthorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Born = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.AuthorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Recipes",
+                columns: table => new
+                {
+                    RecipeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    AuthorId = table.Column<int>(type: "int", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PreparationTime = table.Column<TimeSpan>(type: "time", nullable: false),
+                    TotalTime = table.Column<TimeSpan>(type: "time", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipes", x => x.RecipeId);
+                    table.ForeignKey(
+                        name: "FK_Recipes_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "AuthorId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ingredient",
+                columns: table => new
+                {
+                    IngredientId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    QuantityId = table.Column<int>(type: "int", nullable: false),
+                    RecipeId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ingredient", x => x.IngredientId);
+                    table.ForeignKey(
+                        name: "FK_Ingredient_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "RecipeId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RecipeStep",
+                columns: table => new
+                {
+                    RecipeStepId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RecipeId = table.Column<int>(type: "int", nullable: false),
+                    Instruction = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecipeStep", x => x.RecipeStepId);
+                    table.ForeignKey(
+                        name: "FK_RecipeStep_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "RecipeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "IngredientQuantity",
+                columns: table => new
+                {
+                    IngredientQuantityId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Amount = table.Column<float>(type: "real", nullable: false),
+                    MeasurementUnit = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    IngredientId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngredientQuantity", x => x.IngredientQuantityId);
+                    table.ForeignKey(
+                        name: "FK_IngredientQuantity_Ingredient_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredient",
+                        principalColumn: "IngredientId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RecipeIngredient",
+                columns: table => new
+                {
+                    RecipeId = table.Column<int>(type: "int", nullable: false),
+                    IngredientId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RecipeIngredient", x => new { x.RecipeId, x.IngredientId });
+                    table.ForeignKey(
+                        name: "FK_RecipeIngredient_Ingredient_IngredientId",
+                        column: x => x.IngredientId,
+                        principalTable: "Ingredient",
+                        principalColumn: "IngredientId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RecipeIngredient_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "RecipeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ingredient_RecipeId",
+                table: "Ingredient",
+                column: "RecipeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientQuantity_IngredientId",
+                table: "IngredientQuantity",
+                column: "IngredientId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeIngredient_IngredientId",
+                table: "RecipeIngredient",
+                column: "IngredientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_AuthorId",
+                table: "Recipes",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RecipeStep_RecipeId",
+                table: "RecipeStep",
+                column: "RecipeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IngredientQuantity");
+
+            migrationBuilder.DropTable(
+                name: "RecipeIngredient");
+
+            migrationBuilder.DropTable(
+                name: "RecipeStep");
+
+            migrationBuilder.DropTable(
+                name: "Ingredient");
+
+            migrationBuilder.DropTable(
+                name: "Recipes");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
---
+++ src/FoodRecipesApi.Persistence/Migrations/SQL/UpdateDB.sql
@@ -0,0 +1,83 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [Authors] (
+    [AuthorId] int NOT NULL IDENTITY,
+    [Name] nvarchar(20) NOT NULL,
+    [Surname] nvarchar(20) NOT NULL,
+    [ImageUrl] nvarchar(100) NOT NULL,
+    [Born] datetime2 NOT NULL,
+    CONSTRAINT [PK_Authors] PRIMARY KEY ([AuthorId])
+);
+
+CREATE TABLE [Recipes] (
+    [RecipeId] int NOT NULL IDENTITY,
+    [Title] nvarchar(30) NOT NULL,
+    [Description] nvarchar(500) NOT NULL,
+    [AuthorId] int NOT NULL,
+    [ImageUrl] nvarchar(100) NOT NULL,
+    [PreparationTime] time NOT NULL,
+    [TotalTime] time NOT NULL,
+    CONSTRAINT [PK_Recipes] PRIMARY KEY ([RecipeId]),
+    CONSTRAINT [FK_Recipes_Authors_AuthorId] FOREIGN KEY ([AuthorId]) REFERENCES [Authors] ([AuthorId]) ON DELETE CASCADE
+);
+
+CREATE TABLE [Ingredient] (
+    [IngredientId] int NOT NULL IDENTITY,
+    [Name] nvarchar(50) NOT NULL,
+    [QuantityId] int NOT NULL,
+    [RecipeId] int NULL,
+    CONSTRAINT [PK_Ingredient] PRIMARY KEY ([IngredientId]),
+    CONSTRAINT [FK_Ingredient_Recipes_RecipeId] FOREIGN KEY ([RecipeId]) REFERENCES [Recipes] ([RecipeId])
+);
+
+CREATE TABLE [RecipeStep] (
+    [RecipeStepId] int NOT NULL IDENTITY,
+    [RecipeId] int NOT NULL,
+    [Instruction] nvarchar(500) NOT NULL,
+    [ImageUrl] nvarchar(100) NOT NULL,
+    CONSTRAINT [PK_RecipeStep] PRIMARY KEY ([RecipeStepId]),
+    CONSTRAINT [FK_RecipeStep_Recipes_RecipeId] FOREIGN KEY ([RecipeId]) REFERENCES [Recipes] ([RecipeId]) ON DELETE CASCADE
+);
+
+CREATE TABLE [IngredientQuantity] (
+    [IngredientQuantityId] int NOT NULL IDENTITY,
+    [Amount] real NOT NULL,
+    [MeasurementUnit] nvarchar(20) NOT NULL,
+    [IngredientId] int NOT NULL,
+    CONSTRAINT [PK_IngredientQuantity] PRIMARY KEY ([IngredientQuantityId]),
+    CONSTRAINT [FK_IngredientQuantity_Ingredient_IngredientId] FOREIGN KEY ([IngredientId]) REFERENCES [Ingredient] ([IngredientId]) ON DELETE CASCADE
+);
+
+CREATE TABLE [RecipeIngredient] (
+    [RecipeId] int NOT NULL,
+    [IngredientId] int NOT NULL,
+    CONSTRAINT [PK_RecipeIngredient] PRIMARY KEY ([RecipeId], [IngredientId]),
+    CONSTRAINT [FK_RecipeIngredient_Ingredient_IngredientId] FOREIGN KEY ([IngredientId]) REFERENCES [Ingredient] ([IngredientId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_RecipeIngredient_Recipes_RecipeId] FOREIGN KEY ([RecipeId]) REFERENCES [Recipes] ([RecipeId]) ON DELETE CASCADE
+);
+
+CREATE INDEX [IX_Ingredient_RecipeId] ON [Ingredient] ([RecipeId]);
+
+CREATE UNIQUE INDEX [IX_IngredientQuantity_IngredientId] ON [IngredientQuantity] ([IngredientId]);
+
+CREATE INDEX [IX_RecipeIngredient_IngredientId] ON [RecipeIngredient] ([IngredientId]);
+
+CREATE INDEX [IX_Recipes_AuthorId] ON [Recipes] ([AuthorId]);
+
+CREATE INDEX [IX_RecipeStep_RecipeId] ON [RecipeStep] ([RecipeId]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250810103549_InitialCreate', N'9.0.0');
+
+COMMIT;
+GO
+
