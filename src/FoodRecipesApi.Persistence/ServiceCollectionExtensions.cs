bb02f3443516d3fa848b1ecc4d7aad492ad7cdbe
Michael Shin <mshin1303@gmail.com>
2025-08-10 15:50:15 +1000

    feat: add ServiceCollection Add Persistence extension method

---
+++ src/FoodRecipesApi.Persistence/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+using FoodRecipesApi.Core.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodRecipesApi.Persistence
+{
+    public static class ServiceCollectionExtentions
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connection = configuration.GetConnectionString("FoodRecipesDB")
+                 ?? throw new InvalidOperationException("Missing connection string 'FoodRecipesDB'.");
+
+
+            services.AddDbContext<FoodRecipesDbContext>(options =>
+                options.UseSqlServer(connection));
+
+            services.AddScoped<IFoodRecipesDbContext>(sp => sp.GetRequiredService<FoodRecipesDbContext>());
+
+            return services;
+        }
+
+    }
+}
