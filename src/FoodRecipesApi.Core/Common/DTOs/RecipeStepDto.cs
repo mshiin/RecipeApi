7f01d738ae41d8a6999c990f3f71d10d90c1cb8b
Michael Shin <mshin1303@gmail.com>
2025-08-11 19:10:54 +1000

    main: add recipestepdto

---
+++ src/FoodRecipesApi.Core/Common/DTOs/RecipeStepDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodRecipesApi.Core.Common.DTOs
+{
+    public class RecipeStepDto
+    {
+        public int RecipeStepId { get; set; }
+        public string? Instruction { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
--- src/FoodRecipesApi.Domain/Entities/IngredientQuantity.cs
+++ src/FoodRecipesApi.Domain/Entities/IngredientQuantity.cs
@@ -9,7 +9,7 @@ namespace FoodRecipesApi.Domain.Entities
     {
         public int IngredientQuantityId { get; set; }
         public float Amount { get; set; }
-        public MeasurementUnit? MeasurementUnit { get; set; }
+        public string? MeasurementUnit { get; set; }
         public int MeasurementUnitId { get; set; }
         public Ingredient? Ingredient { get; set; }
         public int IngredientId { get; set; }
