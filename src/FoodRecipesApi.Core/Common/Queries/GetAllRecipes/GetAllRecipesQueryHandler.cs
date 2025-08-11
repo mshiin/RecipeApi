49f4b211809ad60c442ce7af18be81eec78a1a19
Michael Shin <mshin1303@gmail.com>
2025-08-11 20:04:02 +1000

    main: add logging to getallrecipesqueryhandler

--- src/FoodRecipesApi.Core/Common/Queries/GetAllRecipes/GetAllRecipesQueryHandler.cs
+++ src/FoodRecipesApi.Core/Common/Queries/GetAllRecipes/GetAllRecipesQueryHandler.cs
@@ -1,57 +1,68 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using FoodRecipesApi.Core.Common.DTOs;
 using FoodRecipesApi.Core.Common.Interfaces;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace FoodRecipesApi.Core.Common.Queries.GetAllRecipes
 {
     public class GetAllRecipesQueryHandler : IRequestHandler<GetAllRecipesQuery, List<RecipeDto>>
     {
         private readonly IFoodRecipesDbContext _context;
-        public GetAllRecipesQueryHandler(IFoodRecipesDbContext context)
+        private readonly ILogger<GetAllRecipesQueryHandler> _logger;
+        public GetAllRecipesQueryHandler(IFoodRecipesDbContext context, ILogger<GetAllRecipesQueryHandler> logger)
         {
             _context = context;
+            _logger = logger;
         }
-        public async Task<List<RecipeDto>> Handle(GetAllRecipesQuery request, CancellationToken cancellationToken)
+   public async Task<List<RecipeDto>> Handle(GetAllRecipesQuery request, CancellationToken cancellationToken)
         {
-            var recipes = _context.Recipes
-                .AsNoTracking()
-                .Select(r => new RecipeDto
-                {
-                    RecipeId = r.RecipeId,
-                    Title = r.Title,
-                    Description = r.Description,
-                    Author = r.Author.Name + " " + r.Author.Surname,
-                    ImageUrl = r.ImageUrl,
-                    PreparationTimeInMinutes = r.PreparationTimeInMinutes,
-                    TotalTimeInMinutes = r.TotalTimeInMinutes,
-                    RecipeSteps = r.RecipeSteps
-                        .OrderBy(s => s.StepNumber)
-                        .Select(s => new RecipeStepDto
-                        {
-                            RecipeStepId = s.RecipeStepId,
-                            StepNumber = s.StepNumber,
-                            Instruction = s.Instruction,
-                            ImageUrl = s.ImageUrl
-                        }),
-                    RecipeIngredients = r.RecipeIngredients.Select(ri => new IngredientDto
+            try
+            {
+                return await _context.Recipes
+                    .AsNoTracking()
+                    .Select(r => new RecipeDto
                     {
-                        IngredientId = ri.Ingredient.IngredientId,
-                        Name = ri.Ingredient.Name,
-                        Quantity = new IngredientQuantityDto
+                        RecipeId = r.RecipeId,
+                        Title = r.Title,
+                        Description = r.Description,
+                        Author = r.Author.Name + " " + r.Author.Surname,
+                        ImageUrl = r.ImageUrl,
+                        PreparationTimeInMinutes = r.PreparationTimeInMinutes,
+                        TotalTimeInMinutes = r.TotalTimeInMinutes,
+                        RecipeSteps = r.RecipeSteps
+                            .OrderBy(s => s.StepNumber)
+                            .Select(s => new RecipeStepDto
+                            {
+                                RecipeStepId = s.RecipeStepId,
+                                StepNumber = s.StepNumber,
+                                Instruction = s.Instruction,
+                                ImageUrl = s.ImageUrl
+                            }),
+                        RecipeIngredients = r.RecipeIngredients.Select(ri => new IngredientDto
                         {
-                            Amount = ri.Ingredient.Quantity.Amount,
-                            MeasurementUnit = ri.Ingredient.Quantity.MeasurementUnit.Unit
-                        }
+                            IngredientId = ri.Ingredient.IngredientId,
+                            Name = ri.Ingredient.Name,
+                            Quantity = new IngredientQuantityDto
+                            {
+                                Amount = ri.Ingredient.Quantity.Amount,
+                                MeasurementUnit = ri.Ingredient.Quantity.MeasurementUnit.Unit
+                            }
+                        })
                     })
-                });
-
-            return await recipes.ToListAsync(cancellationToken);
-
+                    .ToListAsync(cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to get recipes");
+                throw; 
+            }
         }
+
     }
 }
