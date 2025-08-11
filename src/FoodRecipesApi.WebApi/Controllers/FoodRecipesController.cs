3403cbbfb4bc2fff092270eed59e90c08246ba8e
Michael Shin <mshin1303@gmail.com>
2025-08-11 17:20:00 +1000

    feat: add sql script for db migration from InitialCreate to SetAuthorBornPropColToTypeDate

---
+++ src/FoodRecipesApi.WebApi/Controllers/FoodRecipesController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodRecipesApi.Core.Common.DTOs;
+using FoodRecipesApi.Core.Common.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Update.Internal;
+
+namespace FoodRecipesApi.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FoodRecipesController : ControllerBase
+    {
+        private readonly ILogger _logger;
+        private readonly IFoodRecipesDbContext _context;
+
+        public FoodRecipesController(ILogger<FoodRecipesController> logger, IFoodRecipesDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        [HttpGet]
+        public IEnumerable<RecipeDto> Get()
+        {
+            var recipes = _context.Recipes
+                .Include(r => r.Author)
+                .Include(r => r.RecipeIngredients)
+                    .ThenInclude(ri => ri.Ingredient)
+                        .ThenInclude(i => i.Quantity)
+                            .ThenInclude(q => q.MeasurementUnit)
+                .Include(r => r.RecipeSteps)
+                .Select(r => new RecipeDto()
+                {
+                    RecipeId = r.RecipeId,
+                    Title = r.Title,
+                    Description = r.Description,
+                    Author = $"{r.Author.Name} {r.Author.Surname}",
+                    ImageUrl = r.ImageUrl,
+                    PreparationTimeInMinutes = r.PreparationTimeInMinutes,
+                    TotalTimeInMinutes = r.TotalTimeInMinutes,
+                    RecipeSteps = r.RecipeSteps,
+                    RecipeIngredients = r.RecipeIngredients.Select(ri => new IngredientDto()
+                    {
+                        IngredientId = ri.IngredientId,
+                        Name = ri.Ingredient.Name,
+                        Quantity = new IngredientQuantityDto()
+                        {
+                            Amount = ri.Ingredient.Quantity.Amount,
+                            MeasurementUnit = ri.Ingredient.Quantity.MeasurementUnit
+                        }
+                    })
+                });
+            return recipes.ToList();
+        }
+    }
+}
