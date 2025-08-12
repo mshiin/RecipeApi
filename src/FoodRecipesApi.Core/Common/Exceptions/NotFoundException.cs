7b320c331feca6d7ca5d1928992a4d64b3ed915e
Michael Shin <mshin1303@gmail.com>
2025-08-12 11:40:48 +1000

    main: add NotFoundException

---
+++ src/FoodRecipesApi.Core/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodRecipesApi.Core.Common.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) was not found.")
+        {
+            
+        }
+        
+    }
+}
